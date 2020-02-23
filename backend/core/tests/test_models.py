d3c2212477119e4781174396e1349aa3388876e3
Michał Kowieski <kowieski.m98@gmail.com>
2020-02-23 19:22:41 +0100

    Create test_new_user_email_normalized

--- backend/core/tests/test_models.py
+++ backend/core/tests/test_models.py
@@ -15,3 +15,16 @@ class ModelTests(TestCase):
 
         self.assertEqual(email, user.email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test normalization of user's email address"""
+        unnormalized_email = 'test@GmAiL.com'
+        normalized_email = 'test@gmail.com'
+
+        password = 'password'
+        user = get_user_model().objects.create_user(
+            email=unnormalized_email,
+            password=password
+        )
+
+        self.assertEqual(normalized_email, user.email)
