17a46e7f4d91e37c3698a57a271b2adce5578704
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-14 14:49:47 +0100

    Create 'recipes:ingredient-list' view for backend container

---
+++ backend/recipes/tests/test_ingredients_api.py
@@ -0,0 +1,72 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+from recipes.serializers import IngredientSerializer
+
+
+INGREDIENT_URL = reverse('recipes:ingredient-list')
+
+
+def create_user(email, password):
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password
+    )
+
+
+class PublicIngredientAPITests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        res = self.client.get(INGREDIENT_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientAPITests(TestCase):
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@gmail.com',
+            password='test_password'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_list(self):
+        Ingredient.objects.create(name='Spinash', user=self.user)
+        Ingredient.objects.create(name='Cucumber', user=self.user)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        res = self.client.get(INGREDIENT_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_only_user_ingredients(self):
+        """Test that only user ingredients are retrieved"""
+        Ingredient.objects.create(name='Cucumber', user=self.user)
+        Ingredient.objects.create(name='Tomato', user=self.user)
+
+        other_user = create_user(
+            email='other@gmail.com',
+            password='other_password'
+        )
+        Ingredient.objects.create(name='Broccoli', user=other_user)
+        Ingredient.objects.create(name='Carrot', user=other_user)
+
+        ingredients = Ingredient.objects\
+                                .filter(user=self.user)\
+                                .order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        res = self.client.get(INGREDIENT_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -6,6 +6,7 @@ from recipes import views
 
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 
 app_name = 'recipes'
