ec7af9674630b380a73ff5f5525467410b272fd5
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-22 17:54:01 +0100

    Change code format of "recipes" app tests

--- backend/recipes/tests/test_ingredients_api.py
+++ backend/recipes/tests/test_ingredients_api.py
@@ -8,18 +8,14 @@ from core.models import Ingredient
 from recipes.serializers import IngredientSerializer
 
 
-INGREDIENT_URL = reverse('recipes:ingredient-list')
+INGREDIENT_URL = reverse("recipes:ingredient-list")
 
 
 def create_user(email, password):
-    return get_user_model().objects.create_user(
-        email=email,
-        password=password
-    )
+    return get_user_model().objects.create_user(email=email, password=password)
 
 
 class PublicIngredientAPITests(TestCase):
-
     def setUp(self):
         self.client = APIClient()
 
@@ -30,20 +26,18 @@ class PublicIngredientAPITests(TestCase):
 
 
 class PrivateIngredientAPITests(TestCase):
-
     def setUp(self):
         self.user = create_user(
-            email='test@gmail.com',
-            password='test_password'
+            email="test@gmail.com", password="test_password"
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
     def test_get_list(self):
-        Ingredient.objects.create(name='Spinash', user=self.user)
-        Ingredient.objects.create(name='Cucumber', user=self.user)
+        Ingredient.objects.create(name="Spinash", user=self.user)
+        Ingredient.objects.create(name="Cucumber", user=self.user)
 
-        ingredients = Ingredient.objects.all().order_by('-name')
+        ingredients = Ingredient.objects.all().order_by("-name")
         serializer = IngredientSerializer(ingredients, many=True)
 
         res = self.client.get(INGREDIENT_URL)
@@ -52,19 +46,18 @@ class PrivateIngredientAPITests(TestCase):
 
     def test_get_only_user_ingredients(self):
         """Test that only user ingredients are retrieved"""
-        Ingredient.objects.create(name='Cucumber', user=self.user)
-        Ingredient.objects.create(name='Tomato', user=self.user)
+        Ingredient.objects.create(name="Cucumber", user=self.user)
+        Ingredient.objects.create(name="Tomato", user=self.user)
 
         other_user = create_user(
-            email='other@gmail.com',
-            password='other_password'
+            email="other@gmail.com", password="other_password"
         )
-        Ingredient.objects.create(name='Broccoli', user=other_user)
-        Ingredient.objects.create(name='Carrot', user=other_user)
+        Ingredient.objects.create(name="Broccoli", user=other_user)
+        Ingredient.objects.create(name="Carrot", user=other_user)
 
-        ingredients = Ingredient.objects\
-                                .filter(user=self.user)\
-                                .order_by('-name')
+        ingredients = Ingredient.objects.filter(user=self.user).order_by(
+            "-name"
+        )
         serializer = IngredientSerializer(ingredients, many=True)
 
         res = self.client.get(INGREDIENT_URL)
--- backend/recipes/tests/test_tags_api.py
+++ backend/recipes/tests/test_tags_api.py
@@ -9,14 +9,11 @@ from core.models import Tag
 from recipes.serializers import TagSerializer
 
 
-TAGS_URL = reverse('recipes:tag-list')
+TAGS_URL = reverse("recipes:tag-list")
 
 
 def create_user(email, password):
-    return get_user_model().objects.create_user(
-        email=email,
-        password=password
-    )
+    return get_user_model().objects.create_user(email=email, password=password)
 
 
 class PublicTagsAPITests(TestCase):
@@ -37,18 +34,17 @@ class PrivateTagsAPITests(TestCase):
 
     def setUp(self):
         self.user = get_user_model().objects.create_user(
-            email='email@gmail.com',
-            password='test_password'
+            email="email@gmail.com", password="test_password"
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
     def test_get_tags(self):
         """Test retrieving tags"""
-        Tag.objects.create(name='Dessert', user=self.user)
-        Tag.objects.create(name='Italian', user=self.user)
+        Tag.objects.create(name="Dessert", user=self.user)
+        Tag.objects.create(name="Italian", user=self.user)
 
-        tags = Tag.objects.all().order_by('-name')
+        tags = Tag.objects.all().order_by("-name")
         serializer = TagSerializer(tags, many=True)
 
         res = self.client.get(TAGS_URL)
@@ -57,17 +53,16 @@ class PrivateTagsAPITests(TestCase):
 
     def test_get_only_user_tags(self):
         """Test that only user tags are retrieved"""
-        Tag.objects.create(name='Dessert', user=self.user)
-        Tag.objects.create(name='Italian', user=self.user)
+        Tag.objects.create(name="Dessert", user=self.user)
+        Tag.objects.create(name="Italian", user=self.user)
 
         other_user = create_user(
-            email='other@gmail.com',
-            password='other_password'
+            email="other@gmail.com", password="other_password"
         )
-        Tag.objects.create(name='Vegan', user=other_user)
-        Tag.objects.create(name='Keto', user=other_user)
+        Tag.objects.create(name="Vegan", user=other_user)
+        Tag.objects.create(name="Keto", user=other_user)
 
-        tags = Tag.objects.filter(user=self.user).order_by('-name')
+        tags = Tag.objects.filter(user=self.user).order_by("-name")
         serializer = TagSerializer(tags, many=True)
 
         res = self.client.get(TAGS_URL)
@@ -76,18 +71,15 @@ class PrivateTagsAPITests(TestCase):
 
     def test_create_tag_successful(self):
         """Test creating a tag with valid data."""
-        payload = {'name': 'Vegan'}
+        payload = {"name": "Vegan"}
         self.client.post(TAGS_URL, payload)
 
-        tag = Tag.objects.filter(
-            user=self.user,
-            name=payload['name']
-        )
+        tag = Tag.objects.filter(user=self.user, name=payload["name"])
         self.assertEqual(len(tag), 1)
 
     def test_attemt_to_create_empty_tag(self):
         """Test creating a tag with empty name is unsuccessful."""
-        payload = {'name': ''}
+        payload = {"name": ""}
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
