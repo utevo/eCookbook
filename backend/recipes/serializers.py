02f160b6ecd784dde305753b47d93d27c5826e9d
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-26 00:09:10 +0100

    String normalization of recipes/serializers.py file

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -42,7 +42,7 @@ class RecipeSerializer(serializers.ModelSerializer):
             'ingredients',
             'tags',
         )
-        read_only_fields = ("id", )
+        read_only_fields = ('id', )
 
 
 class RecipeDetailSerializer(RecipeSerializer):
